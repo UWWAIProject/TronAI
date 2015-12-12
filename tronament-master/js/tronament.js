34313684d0cb1e8ff3156240327b3f76f83a164d
NickBolles <nickbolles@live.com>
2015-12-11 18:12:53 -0600

    Fix a contexual this bug by providing a scoped variable for tronament named "that"

--- tronament-master/js/tronament.js
+++ tronament-master/js/tronament.js
@@ -1,30 +1,31 @@
 window.tronament = new function() {
+    var that = this;
     // some constants
-    this.NORTH = 1;
-    this.SOUTH = 2;
-    this.WEST = 3;
-    this.EAST = 4;
-    this.DIRECTIONS = [
-        this.NORTH,
-        this.SOUTH,
-        this.WEST,
-        this.EAST
+    that.NORTH = 1;
+    that.SOUTH = 2;
+    that.WEST = 3;
+    that.EAST = 4;
+    that.DIRECTIONS = [
+        that.NORTH,
+        that.SOUTH,
+        that.WEST,
+        that.EAST
     ]
 
-    this.EMPTY = 0;
-    this.TRAIL = 1;
-    this.OPPONENT = 2;
-    this.WALL = 4;
-    this.OUT_OF_BOUNDS = 8;
+    that.EMPTY = 0;
+    that.TRAIL = 1;
+    that.OPPONENT = 2;
+    that.WALL = 4;
+    that.OUT_OF_BOUNDS = 8;
 
     // game options
-    this.options = {
+    that.options = {
         playerCount: 2,
         fastMovement: false
     }
 
     // some debug options
-    this.debug = {
+    that.debug = {
         // indicates if the framerate should be displayed on the canvas
         showFps: false,
         // the current frame rate
@@ -39,7 +40,7 @@ window.tronament = new function() {
     var canvas, ctx;
 
     // a map of all loaded AI classes
-    this.aiModules = [];
+    that.aiModules = [];
     // an array of currently active players
     var players = [];
     // an array containing the coordinates of each player
@@ -47,7 +48,7 @@ window.tronament = new function() {
     // the dimensions of the board grid
     var boardWidth, boardHeight;
     // a 2d array that stores a collision map for each position on the grid
-    this.collisionMap = [[]];
+    that.collisionMap = [[]];
     var squareWidth, squareHeight;
     var timer;
     var running = false;
@@ -59,10 +60,10 @@ window.tronament = new function() {
      *
      * @param Element canvasElement The canvas element to be used for drawing.
      */
-    this.init = function() {
+    that.init = function() {
         canvas = document.getElementById("canvas");
         ctx = canvas.getContext("2d");
-        this.setBoardSize(48, 48);
+        that.setBoardSize(48, 48);
     }
 
     /**
@@ -71,7 +72,7 @@ window.tronament = new function() {
      * @return Object An object whose "width" property is the width of the board
      *                and "height" property is the height of the board.
      */
-    this.getBoardSize = function() {
+    that.getBoardSize = function() {
         return {
             width: boardWidth,
             height: boardHeight
@@ -84,7 +85,7 @@ window.tronament = new function() {
      * @param Number width  The new board width.
      * @param Number height The new board height.
      */
-    this.setBoardSize = function(width, height) {
+    that.setBoardSize = function(width, height) {
         boardWidth = width;
         boardHeight = height;
 
@@ -97,7 +98,7 @@ window.tronament = new function() {
      * Gets the opposite direction
      * @return direction
      */
-    this.getOppositeDirection = function(direction){
+    that.getOppositeDirection = function(direction){
         switch (direction){
             case tronament.NORTH:
                 return tronament.SOUTH;
@@ -116,20 +117,20 @@ window.tronament = new function() {
      * @return Object An object whose "width" property is the width of the board
      *                and "height" property is the height of the board.
      */
-    this.getPlayerCoordinates = function(i) {
+    that.getPlayerCoordinates = function(i) {
         return playerCoordinates[i];
     };
 
     /**
      * Bootstraps an AI module prototype.
      *
-     * Here there be dragons. This method uses some meta-programming
+     * Here there be dragons. that method uses some meta-programming
      * techniques, so be sure to remember who's who and what's what.
      *
      * @param String   name        The module name.
      * @param Function constructor An object constructor.
      */
-    this.aiModule = function(name, constructor) {
+    that.aiModule = function(name, constructor) {
         constructor.prototype.name = name;
 
         // next, define some useful methods for the module to use...
@@ -146,7 +147,7 @@ window.tronament = new function() {
             var result = tronament.query(x, y);
 
             // do an additional check for opponent trail
-            if (result == tronament.TRAIL && this.collisionMap[x][y] != playerIndex) {
+            if (result == tronament.TRAIL && that.collisionMap[x] && that.collisionMap[x][y] != playerIndex) {
                 result = tronament.OPPONENT;
             }
 
@@ -164,11 +165,11 @@ window.tronament = new function() {
             var playerIndex = players.indexOf(this);
             return this.queryAbsolute(playerCoordinates[playerIndex].x + x, playerCoordinates[playerIndex].y + y);
         }
-		
-		//constructor.prototype.getCurrentPos = function(){
-		//	var positions = [playerCoordinates[playerIndex].x, playerCoordinates[playerIndex].y];
-		//	return positons
-		//}
+
+        //constructor.prototype.getCurrentPos = function(){
+        //	var positions = [playerCoordinates[playerIndex].x, playerCoordinates[playerIndex].y];
+        //	return positons
+        //}
 
         /**
          * Displays a fun message in the Tronament UI.
@@ -182,16 +183,16 @@ window.tronament = new function() {
             messageBox.value = message;
         }
 
-        this.aiModules[name] = constructor;
+        that.aiModules[name] = constructor;
         lastLoadedModule = name;
     }
 
     /**
      * Loads a module from file.
      */
-    this.loadModule = function() {
+    that.loadModule = function() {
         // open the file chooser
-        this.ui.openFilePicker(function(files) {
+        that.ui.openFilePicker(function(files) {
             // get the chosen file
             var file = files[0];
             var objectURL = window.URL.createObjectURL(file);
@@ -203,9 +204,9 @@ window.tronament = new function() {
 
             // update the player widgets after the module is loaded
             script.onload = function() {
-                this.ui.updatePlayerWidgets();
-            }.bind(this);
-        }.bind(this));
+                that.ui.updatePlayerWidgets();
+            }.bind(that);
+        }.bind(that));
     }
 
     /**
@@ -214,7 +215,7 @@ window.tronament = new function() {
      * @param Number width  The new canvas width.
      * @param Number height The new canvas height.
      */
-    this.setResolution = function(width, height) {
+    that.setResolution = function(width, height) {
         canvas.width = width;
         canvas.height = height;
     }
@@ -226,22 +227,22 @@ window.tronament = new function() {
      * @param  Number  y The y-coordinate of the position.
      * @return Number    A Space value indicating what is in the space.
      */
-    this.query = function(x, y) {
+    that.query = function(x, y) {
         if (x < 0 || x >= boardWidth || y < 0 || y >= boardHeight) {
-            return this.OUT_OF_BOUNDS;
+            return that.OUT_OF_BOUNDS;
         }
-        if (this.collisionMap[x] != undefined && this.collisionMap[x][y] != undefined) {
-            return this.TRAIL;
+        if (that.collisionMap[x] != undefined && that.collisionMap[x][y] != undefined) {
+            return that.TRAIL;
         }
-        return this.EMPTY;
+        return that.EMPTY;
     }
 
     /**
      * Resets all the game variables and starts a new game running.
      */
-    this.start = function() {
+    that.start = function() {
         players = [];
-        this.collisionMap = [[]];
+        that.collisionMap = [[]];
         playerCoordinates = [
             { x: Math.floor(boardWidth * 0.1), y: Math.floor(boardHeight * 0.1) },
             { x: Math.floor(boardWidth * 0.9), y: Math.floor(boardHeight * 0.9) },
@@ -250,14 +251,15 @@ window.tronament = new function() {
         ];
 
         // create player objects
-        for (var i = 0; i < this.options.playerCount; i++) {
-            // get the desired a.i. module for this player
+        for (var i = 0; i < that.options.playerCount; i++) {
+            // get the desired a.i. module for that player
             var name = document.getElementById("player-ai-" + (i + 1)).value;
 
             // initialize an a.i. module instance
             try {
-                var instance = new this.aiModules[name]();
+                var instance = new that.aiModules[name]();
             } catch(e) {
+                console.log(e);
                 tronament.ui.showDialog("Error", "The A.I. \"" + name + "\" created the following exception: " + e.message);
                 return;
             }
@@ -284,7 +286,7 @@ window.tronament = new function() {
     /**
      * Ends the game.
      */
-    this.end = function(player) {
+    that.end = function(player) {
         terminateLoop();
         tronament.ui.showDialog("Game Over", "Player " + (players.indexOf(player) + 1) + " (" + player.name + ") Wins!", true);
         tronament.ui.playAudio("sound2");
@@ -315,12 +317,12 @@ window.tronament = new function() {
         // update fps every second
         if (now - lastSecondTime > 1000) {
             var delta = (now - lastDrawTime) / 1000;
-            this.debug.fps = 1 / delta;
+            that.debug.fps = 1 / delta;
             lastSecondTime = now;
         }
 
         // check if it is time to run the next game tick based on speed setting
-        if (this.options.fastMovement || now - lastTickTime > 100) {
+        if (that.options.fastMovement || now - lastTickTime > 100) {
             tick();
             lastTickTime = now; // update tick time since we just called it
         }
@@ -328,7 +330,7 @@ window.tronament = new function() {
         // render the display canvas
         draw();
         lastDrawTime = now;
-    }.bind(this);
+    }.bind(that);
 
     var timestamp = function() {
         return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
@@ -347,31 +349,32 @@ window.tronament = new function() {
 
             // tell the player to move
             try {
-                var move = players[i].move(this);
+                var move = players[i].move(that);
             } catch(e) {
+                console.log(e);
                 tronament.ui.showDialog("Error", "The A.I. \"" + players[i].name + "\" created the following exception: " + e.message);
                 terminateLoop();
                 return;
             }
 
             // adjust the player position based on the direction
-            if (move == this.EAST) {
+            if (move == that.EAST) {
                 playerCoordinates[i].x++;
-            } else if (move == this.SOUTH) {
+            } else if (move == that.SOUTH) {
                 playerCoordinates[i].y++;
-            } else if (move == this.WEST) {
+            } else if (move == that.WEST) {
                 playerCoordinates[i].x--;
-            } else if (move == this.NORTH) {
+            } else if (move == that.NORTH) {
                 playerCoordinates[i].y--;
             }
 
-            if (this.query(playerCoordinates[i].x, playerCoordinates[i].y)) {
+            if (that.query(playerCoordinates[i].x, playerCoordinates[i].y)) {
                 playerDeath(i);
             } else {
                 fill(i, playerCoordinates[i].x, playerCoordinates[i].y);
             }
         }
-    }.bind(this);
+    }.bind(that);
 
     /**
      * Fills a player's position into the collision map.
@@ -380,11 +383,11 @@ window.tronament = new function() {
      * @param Number y The y-coordinate of the position.
      */
     var fill = function(playerIndex, x, y) {
-        if (this.collisionMap[x] == undefined) {
-            this.collisionMap[x] = [];
+        if (that.collisionMap[x] == undefined) {
+            that.collisionMap[x] = [];
         }
-        this.collisionMap[x][y] = playerIndex;
-    }.bind(this);
+        that.collisionMap[x][y] = playerIndex;
+    }.bind(that);
 
     /**
      * Removes a player from the game.
@@ -402,11 +405,11 @@ window.tronament = new function() {
         if (alivePlayerCount <= 1) {
             for (var i = 0; i < players.length; i++) {
                 if (!players[i].__dead) {
-                    this.end(players[i]);
+                    that.end(players[i]);
                 }
             }
         }
-    }.bind(this);
+    }.bind(that);
 
     /**
      * Renders the game screen to the canvas.
@@ -418,11 +421,11 @@ window.tronament = new function() {
         // calculate the size of the trails
 
         // render all trails
-        for (var x = 0; x < this.collisionMap.length; x++) {
-            if (this.collisionMap[x] != undefined) {
-                for (var y = 0; y < this.collisionMap[x].length; y++) {
-                    if (this.collisionMap[x][y] != undefined) {
-                        var player = players[this.collisionMap[x][y]];
+        for (var x = 0; x < that.collisionMap.length; x++) {
+            if (that.collisionMap[x] != undefined) {
+                for (var y = 0; y < that.collisionMap[x].length; y++) {
+                    if (that.collisionMap[x][y] != undefined) {
+                        var player = players[that.collisionMap[x][y]];
                         ctx.fillStyle = player.color;
                         ctx.fillRect(x * squareWidth, y * squareHeight, squareWidth, squareHeight);
                     }
@@ -444,12 +447,12 @@ window.tronament = new function() {
         ctx.stroke();
 
         // render debug info
-        if (this.debug.showFps) {
+        if (that.debug.showFps) {
             ctx.fillStyle = "white";
             ctx.font = "24px Silkscreen";
             ctx.textAlign = "end";
             ctx.textBaseline = "bottom";
-            ctx.fillText("FPS: " + this.debug.fps.toFixed(2), canvas.width - 10, canvas.height - 10);
+            ctx.fillText("FPS: " + that.debug.fps.toFixed(2), canvas.width - 10, canvas.height - 10);
         }
-    }.bind(this);
+    }.bind(that);
 };
